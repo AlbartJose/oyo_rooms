48b19c85dcd697399339645e58e1a3cebcf9dcba
akifpatel <aakifpatel08@gmail.com>
2021-09-28 13:23:13 +0530

    inital setup

--- oyorooms-react-clone/src/App.js
+++ oyorooms-react-clone/src/App.js
@@ -1,9 +1,11 @@
-
+import {Card} from "./components/Card"
 
 function App() {
   return (
     <div>
       Start Here
+      {/* cards */}
+      <Card/>
       {/* Comments Everything You write in this file */}
     </div>
   );
---
+++ oyorooms-react-clone/src/components/Card.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+
+export function Card(){
+return <>
+
+    </>
+}
