e23c66d2ae6fa661a5a5103b4508fa7cc8b6f086
Salman Shaikh <salman_nj2_107@masai.school>
2021-10-01 19:19:52 +0530

    calender working

--- oyorooms-react-clone/src/Components/Home/SearchField.jsx
+++ oyorooms-react-clone/src/Components/Home/SearchField.jsx
@@ -48,11 +48,9 @@ function SearchField() {
           <RangeDatePicker
             startDate={new Date(2020, 0, 15)}
             endDate={new Date(2020, 1, 1)}
-            onChange={(startDate, endDate) =>
-              setonDateChange(startDate, endDate)
-            }
-            minDate={new Date(1900, 0, 1)}
-            maxDate={new Date(2100, 0, 1)}
+            // onChange={(startDate, endDate) =>
+            //   setonDateChange(startDate, endDate)
+            // }
           />
           <div className="rooms" onClick={handleRoomDIv}>
             <p>1 Room, 1 Guest</p>
